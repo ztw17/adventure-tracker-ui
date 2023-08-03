332dfbacb4d0887906bccf9ffe50ae6df8d95bac
Zach Weber <weberzt@gmail.com>
2023-08-03 15:47:36 -0600

    Able to route to login component

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,20 +1,28 @@
 import React, { FC } from 'react';
 import { NavigationBarWrapper } from './NavigationBar.styled';
 import mainLogo from '../../assets/icons/mountain.png';
+import { useNavigate } from 'react-router-dom';
 
 interface NavigationBarProps {}
 
-const NavigationBar: FC<NavigationBarProps> = () => (
- <NavigationBarWrapper data-testid="NavigationBar">
-    <div className='logo-wrapper'>
-      <img src={mainLogo} className='logo'></img>
-      <div className='title font-link'>Adventure Tracker</div>
-    </div>
-    <div className='buttons-wrapper'>
-      <div className='login-button clickable center-buttons'>Log in</div>
-      <div className='sign-up-button clickable center-buttons'>Sign up</div>
-    </div>
- </NavigationBarWrapper>
-);
+const NavigationBar: FC<NavigationBarProps> = () => {
+  const navigate = useNavigate();
+  const handleLogInClick = () => {
+    navigate('/login')
+  }
+
+  return (
+  <NavigationBarWrapper data-testid="NavigationBar">
+      <div className='logo-wrapper'>
+        <img src={mainLogo} className='logo'></img>
+        <div className='title font-link'>Adventure Tracker</div>
+      </div>
+      <div className='buttons-wrapper'>
+        <div onClick={handleLogInClick} className='login-button clickable center-buttons'>Log in</div>
+        <div className='sign-up-button clickable center-buttons'>Sign up</div>
+      </div>
+  </NavigationBarWrapper>
+  );
+}
 
 export default NavigationBar;
