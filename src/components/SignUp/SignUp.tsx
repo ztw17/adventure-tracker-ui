c09899889e1dad50c5846dbbd98976efd6ad15ec
Zach Weber <weberzt@gmail.com>
2023-08-08 14:04:38 -0600

    Begin sign up form

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
 import { SignUpWrapper } from './SignUp.styled';
+import { TextField } from '@mui/material';
 
 interface SignUpProps {}
 
 const SignUp: FC<SignUpProps> = () => (
  <SignUpWrapper data-testid="SignUp">
-    SignUp Component
+   <div>
+      <TextField label="First name" variant="outlined" />
+      <TextField label="Last name" variant="outlined" />
+      <TextField label="Email address" variant="outlined" />
+      <TextField label="Password" variant="outlined" />
+   </div>
  </SignUpWrapper>
 );
 
