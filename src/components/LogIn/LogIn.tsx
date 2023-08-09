29de00857b134375a709c71e5008331758f91639
Zach Weber <weberzt@gmail.com>
2023-08-09 10:39:42 -0600

    Begin login form

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
 import { LogInWrapper } from './LogIn.styled';
+import { TextField } from '@mui/material';
 
 interface LogInProps {}
 
 const LogInPage: FC<LogInProps> = () => (
  <LogInWrapper data-testid="LogIn">
-    LogIn Component
+   <div className="form-wrapper">
+      <div className="form-card">
+         <TextField label="Email address" variant="outlined" />
+         <TextField label="Password" variant="outlined" />
+      </div>
+   </div>
  </LogInWrapper>
 );
 
